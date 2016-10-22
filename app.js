e414a9cfe8ecfd194d4bb7101ce98b54da0f7d25
ldwgulden <ldw_weit@yahoo.com>
2016-10-22 15:42:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '2109fd04-b4c0-4689-9b2c-a47feb2550d1',  // <----- EDIT ME!
-  redirect_uri: 'https://guldenlotto.github.io',
+  redirect_uri: 'https://ldwgulden.github.io/guldenlotto.github.io/',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
